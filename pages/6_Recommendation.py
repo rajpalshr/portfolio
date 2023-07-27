6b9f91deffd380bae973739cee67a603ae9ad48c
rajpalshr <140659980+rajpalshr@users.noreply.github.com>
2023-07-27 01:56:45 -0400

    Update 6_Recommendation.py

--- pages/6_Recommendation.py
+++ pages/6_Recommendation.py
@@ -25,6 +25,14 @@ from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import Pipeline
 ######################
 
+
+# Page
+######################
+st.set_page_config(
+    page_title="Movie Recommendation"
+)
+######################
+
 # Header, Footer
 ######################
 hide_st_style = """
@@ -37,13 +45,6 @@ hide_st_style = """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 ######################
 
-
-# Page
-######################
-st.set_page_config(
-    page_title="Movie Recommendation"
-)
-######################
 
 # Title
 ######################
